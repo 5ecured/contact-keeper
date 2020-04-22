a4001cb058f4c2abdba4d84d3357089e24041713
Edward Ottorino <edward.ottorino@gmail.com>
2020-04-22 16:25:58 +1000

    'test'

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const connectDB = require('./config/db')
-// const path = require('path')
+const path = require('path')
 
 connectDB()
 
@@ -11,10 +11,10 @@ app.use('/api/auth', require('./routes/auth'))
 app.use('/api/contacts', require('./routes/contacts'))
 app.use(express.static('client/build'))
 
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static('client/build'))
-//     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
-// }
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+}
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
